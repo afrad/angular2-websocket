ff65a8263a47e16de568783739edd364141f0942
jmparra <juan.parraguez@alumnos.usm.cl>
2016-05-10 07:07:22 -0300

    fixed: Upgrade to Angular2.0.0-rc.1
    * fixed dependencies to upgrade to Angular2.0.0-rc.1
    
    added readme instruction to compile
    
    new version compiled
    
    update dependencies and compiled js
    
    * update fixup

--- angular2-websocket.js
+++ angular2-websocket.js
@@ -1,3 +1,4 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -7,8 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('angular2/core');
-var lang_1 = require('angular2/src/facade/lang');
+var core_1 = require('@angular/core');
+var lang_1 = require('@angular/compiler/src/facade/lang');
 var Subject_1 = require("rxjs/Subject");
 var $WebSocket = (function () {
     function $WebSocket(url, protocols, config) {
@@ -203,5 +204,6 @@ var $WebSocket = (function () {
         __metadata('design:paramtypes', [String, Array, Object])
     ], $WebSocket);
     return $WebSocket;
-})();
+}());
 exports.$WebSocket = $WebSocket;
+//# sourceMappingURL=angular2-websocket.js.map
