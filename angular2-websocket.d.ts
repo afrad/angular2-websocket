908aa55f87dc312c0d97546a9f042890b8e45f78
LyokoJeremie <lucheng989898@gmail.com>
2016-11-16 01:09:54 +0800

    fix send, fix lost data before connect open, add example. (#49)
    * change send returning from cold Observable to 3 mode
    1(Direct) send and return true or false (false when socket is close)
    2(Promise) send and return promise
    3(Observable) send and return cold Observable
    
    the default mode is 1(Direct).
    
    and as soon as that, import bluebird
    
    * change send returning from cold Observable to 3 mode
    1(Direct) send and return true or false (false when socket is close)
    2(Promise) send and return promise
    3(Observable) send and return cold Observable
    
    the default mode is 3(Observable).
    
    and as soon as that, update compile target to es6 and import bluebird
    
    * update typescript version to 2.1.1 to fix typescript server compile error on webstorm
    
    * fix
    
    * fix data lose when connect creating before connect open.
    now you can connect and immediately send data!
    the data will cache in local until connect create or lost, cached data will send in connect create immediately!
    every connect close in not reconnect mode will clear cached data, but reconnect will not.
    
    * enum change name
    
    * add example
    
    * small change

--- angular2-websocket.d.ts
+++ angular2-websocket.d.ts
@@ -1,14 +1,10 @@
-import { Observable } from "rxjs/Observable";
-import { Subject } from "rxjs/Subject";
-export interface WebSocketConfig {
-    initialTimeout: number;
-    maxTimeout: number;
-    reconnectIfNotNormalClose: boolean;
-}
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 export declare class $WebSocket {
     private url;
     private protocols;
     private config;
+    private static Helpers;
     private reconnectAttempts;
     private sendQueue;
     private onOpenCallbacks;
@@ -19,10 +15,48 @@ export declare class $WebSocket {
     private normalCloseCode;
     private reconnectableStatusCodes;
     private socket;
+    private dataStream;
     private internalConnectionState;
     constructor(url: string, protocols?: Array<string>, config?: WebSocketConfig);
     connect(force?: boolean): void;
-    static create(url: string, protocols: Array<string>): WebSocket;
+    /**
+     * Run in Block Mode
+     * Return true when can send and false in socket closed
+     * @param data
+     * @returns {boolean}
+     */
+    send4Direct(data: any): boolean;
+    /**
+     * Return Promise
+     * When can Send will resolve Promise
+     * When Socket closed will reject Promise
+     * @param data
+     * @returns {Promise<any>}
+     */
+    send4Promise(data: any): Promise<any>;
+    /**
+     * Return cold Observable
+     * When can Send will complete observer
+     * When Socket closed will error observer
+     * @param data
+     * @returns {Observable<any>}
+     */
+    send4Observable(data: any): Observable<any>;
+    private send4Mode;
+    /**
+     * Set send(data) function return mode
+     * @param mode
+     */
+    setSend4Mode(mode: WebSocketSendMode): void;
+    /**
+     * Use {mode} mode to send {data} data
+     * If no specify, Default SendMode is Observable mode
+     * @param data
+     * @param mode
+     * @returns {any}
+     */
+    send(data: any, mode?: WebSocketSendMode): any;
+    getDataStream(): Subject<any>;
     onOpenHandler(event: Event): void;
     notifyOpenCallbacks(event: any): void;
     fireQueue(): void;
@@ -31,16 +65,27 @@ export declare class $WebSocket {
     onOpen(cb: any): this;
     onClose(cb: any): this;
     onError(cb: any): this;
-    onMessage(callback: any, options: any): this;
+    onMessage(callback: any, options?: any): this;
     onMessageHandler(message: MessageEvent): void;
     onCloseHandler(event: CloseEvent): void;
     onErrorHandler(event: any): void;
-    send(data: any): Observable<any>;
     reconnect(): this;
-    close(force: boolean): this;
+    close(force?: boolean): this;
     getBackoffDelay(attempt: any): number;
     setInternalState(state: any): void;
+    /**
+     * Could be -1 if not initzialized yet
+     * @returns {number}
+     */
     getReadyState(): number;
-    setReadyState(): void;
-    getDataStream(): Subject<any>;
+}
+export interface WebSocketConfig {
+    initialTimeout: number;
+    maxTimeout: number;
+    reconnectIfNotNormalClose: boolean;
+}
+export declare enum WebSocketSendMode {
+    Direct = 0,
+    Promise = 1,
+    Observable = 2,
 }
